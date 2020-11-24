a148d9dbae5345eea52ab8690ae1c803ab35aa9a
Frank.Wu <Frank.Wu@Bionime>
2020-11-24 11:18:38 +0800

    Create Constructor

--- lib/words.dart
+++ lib/words.dart
@@ -4,6 +4,12 @@ class Words {
   String name;
   bool highlight;
 
+
+  Words(String name,bool highlight){
+    this.name = name;
+    this.highlight = highlight;
+  }
+
   Words.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     highlight = json['highlight'];
